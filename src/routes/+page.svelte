615eabe1da9590013fc1ae11cff5e3190129d916
Thomas Zerr <thomas.zerr@gmail.com>
2023-01-24 17:20:59 +0100

    -added: first tutorial chapter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import Nested from "../components/Nested.svelte";
+
+  let name = "Thomas"
+  let src = "/favicon.png"
+  let alt = "Svelte favicon"
+  let htmlMarkup = `this string contains some <strong>HTML!!!</strong>`;
+</script>
+
+<h1>Welcome, {name}</h1>
+<img {src} {alt}>
+<p>This is a paragraph</p>
+<Nested />
+<!-- Achtung vor XSS wenn der Inhalt nicht vetrauenswürdig ist -->
+<p>{@html htmlMarkup}</p>
+
+
+<style>
+  p {
+    color: purple;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+  }
+</style>
